4aac6d964403c8545b33d855fd428fd5902fd3a8
Peter Jang <peter.x.jang@gmail.com>
2019-12-22 10:55:33 -0600

    Define note selector components using data with props

--- src/components/NoteSelectors.vue
+++ src/components/NoteSelectors.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="note-selectors">
-    <note-selector />
-    <note-selector />
-    <note-selector />
-    <note-selector />
-    <note-selector />
+    <note-selector v-for="note in notes" v-bind:key="note.id" v-bind:note="note" />
   </div>
 </template>
 
@@ -13,6 +9,16 @@ import NoteSelector from "./NoteSelector";
 
 export default {
   name: "note-selectors",
-  components: { NoteSelector }
+  components: { NoteSelector },
+  data: function() {
+    return {
+      notes: [
+        { id: 1, body: "This is a first test", timestamp: Date.now() },
+        { id: 2, body: "This is a second test", timestamp: Date.now() },
+        { id: 3, body: "This is a third test", timestamp: Date.now() },
+        { id: 4, body: "This is a fourth test", timestamp: Date.now() }
+      ]
+    };
+  }
 };
 </script>
